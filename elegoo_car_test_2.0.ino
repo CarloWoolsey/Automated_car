f04a5fbb0447d41886728e0bed3fb565032d7320
CarloWoolsey <73916466+CarloWoolsey@users.noreply.github.com>
2020-11-04 14:02:25 -0800

    autonomous car reformat
    adding the edits suggested by matt

--- elegoo_car_test_2.0.ino
+++ elegoo_car_test_2.0.ino
@@ -30,106 +30,65 @@ void setup() {
   digitalWrite(ln2, LOW);
   digitalWrite(ln3, LOW);
   digitalWrite(ln4, HIGH);
-  Serial.begin(9600);
   
 
 
 }
 
 void loop() {
+  int switchState = digitalRead(16);
+  if(switchState == LOW){
+    autoState = true;
+  }
   if(autoState == false){
-    int dataForRightMotor = getMotorSpeedRight();
-    int dataForLeftMotor = getMotorSpeedLeft();
-    analogWrite(enA, dataForRightMotor);
-    analogWrite(enB, dataForLeftMotor);
-    myVector1.push_back(dataForRightMotor);
-    myVector2.push_back(dataForLeftMotor);
-    delay(5);
-    switchState = digitalRead(16);
-    if(switchState == LOW){
-      autoState = true;
-    }
-    
+    record();   
   }
   if(autoState == true){
-    
-  
-    if (index < myVector1.size()){
-//      Serial.print("myVector1: ");
-//      Serial.print(myVector1[index]);
-//      Serial.print("myVector2: ");
-//      Serial.print(myVector2[index]);
-//      Serial.print(" index: ");
-//      Serial.println(index);
+    if(index < myVector1.size()){
       analogWrite(enA, myVector1[index]);
       analogWrite(enB, myVector2[index]);
       index++;
-      delay(5);
-//    for(size_t i = 0; i < myVector1.size(); i ++){
-//      analogWrite(enA, myVector1[i]);     
-//    }
-//    for(size_t j = 0; j < myVector2.size(); j++){
-//      analogWrite(enB, myVector2[j]);
-//    }
-    if(switchState == LOW){
-      autoState = false;
-    }
+      delay(5);      
+    }    
   }
 }
-  
-  
 
-
-}
-
-int getMotorSpeedRight(){  
+void record(){  
     int yAxisSpeed = analogRead(vry);
     int xAxisSpeed = analogRead(vrx);
     if (yAxisSpeed > 550){
     motorSpeedRight = map(yAxisSpeed, 550, 1023, 0, 255);
+    motorSpeedLeft = map(yAxisSpeed, 550, 1023, 0, 255);
     }
     else{
       motorSpeedRight = 0;
+      motorSpeedLeft = 0;
     }
      if (xAxisSpeed <470){
       int xMapped = map(xAxisSpeed, 470,0,0,255);
       motorSpeedRight = motorSpeedRight - xMapped;
+      motorSpeedLeft = motorSpeedLeft + xMapped;
       if(motorSpeedRight >255){
        motorSpeedRight = 255;
+      }
+      if(motorSpeedLeft < 0){
+       motorSpeedLeft = 0;
       }
     } 
     if(xAxisSpeed >550){
       int xMapped = map(xAxisSpeed, 550, 1023, 0, 255);
       motorSpeedRight = motorSpeedRight + xMapped;
+      motorSpeedLeft = motorSpeedLeft - xMapped;
       if(motorSpeedRight < 0){
         motorSpeedRight = 0;
       }
-    }
-    return motorSpeedRight;
-}
-int getMotorSpeedLeft(){
-  int yAxisSpeed = analogRead(vry);
-  int xAxisSpeed = analogRead(vrx);
-  if (yAxisSpeed > 550){
-   motorSpeedLeft = map(yAxisSpeed, 550, 1023, 0, 255);
-  }
-  else{
-    motorSpeedLeft = 0;
-  }
-  if (xAxisSpeed <470){
-    int xMapped = map(xAxisSpeed, 470,0,0,255);
-    motorSpeedLeft = motorSpeedLeft + xMapped;
-    if(motorSpeedLeft < 0){
-      motorSpeedLeft = 0;
-    }
-  }
-  if(xAxisSpeed >550){
-    int xMapped = map(xAxisSpeed, 550, 1023, 0, 255);
-    motorSpeedLeft = motorSpeedLeft - xMapped;
-    if(motorSpeedLeft > 255){
+     if(motorSpeedLeft > 255){
       motorSpeedLeft = 255;
+     }
     }
-  }
-  return motorSpeedLeft;
-  
+    analogWrite(enA, motorSpeedRight);
+    analogWrite(enB, motorSpeedLeft);
+    myVector1.push_back(motorSpeedRight);
+    myVector2.push_back(motorSpeedLeft);
+    
 }
